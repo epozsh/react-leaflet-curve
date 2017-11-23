83dbd4083f0ce6010e0f1a4da5011c50bf2b82c0
Christiano Milfont <cmilfont@gmail.com>
2017-11-23 13:49:25 -0200

    [#master] spliting PropTypes from React to new prop-types package

--- src/Curve.js
+++ src/Curve.js
@@ -1,4 +1,5 @@
-import React, { PropTypes } from 'react';
+import React from 'react';
+import PropTypes from 'prop-types';
 import { Path } from 'react-leaflet';
 import { curve } from './leaflet.curve';
 
